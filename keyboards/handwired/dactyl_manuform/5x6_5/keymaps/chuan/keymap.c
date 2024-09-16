b454092781208cba478647aecb17a9355533f96e
chuan2984 <chuanscareer@gmail.com>
2024-09-16 11:13:54 -0700

    feat: replace reggular HM mods by a custom sm_td mod

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/chuan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/chuan/keymap.c
@@ -24,39 +24,69 @@
 
 // layer declarations
 enum custom_layers {
-  _CHUAN = 0,
-  _SYMBOL = 1,
-  _EXTEND = 2,
+    _CHUAN = 0,
+    _SYMBOL = 1,
+    _EXTEND = 2,
 };
 
 enum custom_keycodes {
-  INV_1P = SAFE_RANGE,
-  DB_COLN,
-  PARENT_DIR,
+    INV_1P = SAFE_RANGE,
+    DB_COLN,
+    PARENT_DIR,
+    SMTD_KEYCODES_BEGIN,
+    CKC_A, // reads as C(ustom) + KC_A, but you may give any name here
+    CKC_S,
+    CKC_D,
+    CKC_F,
+    CKC_J,
+    CKC_K,
+    CKC_L,
+    CKC_SCLN,
+    SMTD_KEYCODES_END,
 };
+
+// needs to be after custom_keycode declariation according to sm_td
+#include "sm_td.h"
 
 // define keycodes
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case INV_1P:
-      if (record->event.pressed) {
-        SEND_STRING(SS_DOWN(X_LSFT) SS_DOWN(X_LCTL) SS_TAP(X_X) SS_UP(X_LCTL) SS_UP(X_LSFT));
-      }
-      return false;
-    case DB_COLN:
-      if (record->event.pressed) {
-        SEND_STRING("::");
-      }
-      return false;
-    case PARENT_DIR:
-      if (record->event.pressed) {
-        SEND_STRING("../");
-      }
-      return false;
-    default:
-      return true;
-  }
+    if (!process_smtd(keycode, record)) {
+        return false;
+    }
+
+    switch (keycode) {
+        case INV_1P:
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LSFT) SS_DOWN(X_LCTL) SS_TAP(X_X) SS_UP(X_LCTL) SS_UP(X_LSFT));
+            }
+            return false;
+        case DB_COLN:
+            if (record->event.pressed) {
+                SEND_STRING("::");
+            }
+            return false;
+        case PARENT_DIR:
+            if (record->event.pressed) {
+                SEND_STRING("../");
+            }
+            return false;
+        default:
+            return true;
+    }
 };
+
+void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
+    switch (keycode) {
+        SMTD_MT(CKC_A, KC_A, KC_LEFT_GUI)
+        SMTD_MT(CKC_S, KC_S, KC_LEFT_ALT)
+        SMTD_MT(CKC_D, KC_D, KC_LSFT)
+        SMTD_MT(CKC_F, KC_F, KC_LEFT_CTRL)
+        SMTD_MT(CKC_J, KC_J, KC_RIGHT_CTRL)
+        SMTD_MT(CKC_K, KC_K, KC_RIGHT_SHIFT)
+        SMTD_MT(CKC_L, KC_L, KC_LEFT_ALT)
+        SMTD_MT(CKC_SCLN, KC_SCLN, KC_RIGHT_GUI)
+    }
+}
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -71,32 +101,32 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_CHUAN] = LAYOUT_5x6_5(
-    INV_1P,      KC_1,      KC_2,        KC_3,          KC_4,          KC_5,                                                                    KC_6,        KC_7,      KC_8,         KC_9,       KC_0,        KC_NO,
-    KC_TAB,      KC_Q,      KC_W,        KC_E,          KC_R,          KC_T,                                                                    KC_Y,        KC_U,      KC_I,         KC_O,       KC_P,        KC_UNDS,
-    KC_ESC,      GUI_A,     ALT_S,       SFT_D,         CTL_F,         KC_G,                                                                    KC_H,        CTL_J,     SFT_K,        ALT_L,      GUI_SCLN,    KC_QUOT,
-    KC_LSFT,     KC_Z,      KC_X,        KC_C,          KC_V,          KC_B,                                                                    KC_N,        KC_M,      KC_COMM,      KC_DOT,     KC_SLSH,     KC_RSFT,
-                            KC_RBRC,     KC_PERC,                      KC_BSPC,       KC_LGUI,    TT(_EXTEND),       TT(_EXTEND),    KC_LCTL,   KC_SPC,                 KC_EQL,       KC_DEL,
-                                                                                      KC_LALT,    TT(_SYMBOL),       TT(_SYMBOL),    KC_ENT
-  ),
+    [_CHUAN] = LAYOUT_5x6_5(
+        INV_1P,      KC_1,      KC_2,        KC_3,          KC_4,          KC_5,                                                                         KC_6,        KC_7,      KC_8,         KC_9,       KC_0,        KC_NO,
+        KC_TAB,      KC_Q,      KC_W,        KC_E,          KC_R,          KC_T,                                                                         KC_Y,        KC_U,      KC_I,         KC_O,       KC_P,        KC_UNDS,
+        KC_ESC,      CKC_A,     CKC_S,       CKC_D,         CKC_F,         KC_G,                                                                         KC_H,        CKC_J,     CKC_K,        CKC_L,      CKC_SCLN,    KC_QUOT,
+        KC_LSFT,     KC_Z,      KC_X,        KC_C,          KC_V,          KC_B,                                                                         KC_N,        KC_M,      KC_COMM,      KC_DOT,     KC_SLSH,     KC_RSFT,
+                                KC_RBRC,     KC_PERC,                      KC_BSPC,     KC_LGUI,       TT(_EXTEND),       TT(_EXTEND),    KC_LCTL,       KC_SPC,                 KC_EQL,       KC_DEL,
+                                                                                        KC_LALT,       TT(_SYMBOL),       TT(_SYMBOL),    KC_ENT
+    ),
 
-  [_SYMBOL] = LAYOUT_5x6_5(
-    KC_TRNS,    KC_F1,      KC_F2,        KC_F3,        KC_F4,         KC_F5,                                                                   KC_F6,      KC_F7,      KC_F8,        KC_F9,      KC_F10,      KC_TRNS,
-    KC_TRNS,    KC_QUOT,    KC_LT,        KC_GT,        KC_DQUO,       KC_HASH,                                                                 KC_AMPR,    DB_COLN,    KC_LBRC,      KC_RBRC,    KC_PERC,     KC_TRNS,
-    KC_TRNS,    KC_EXLM,    KC_MINS,      KC_PLUS,      KC_EQL,        KC_GRV,                                                                  KC_PIPE,    KC_COLN,    KC_LPRN,      KC_RPRN,    KC_QUES,     KC_TRNS,
-    KC_TRNS,    KC_CIRC,    KC_SLSH,      KC_ASTR,      KC_BSLS,       PARENT_DIR,                                                              KC_TILD,    KC_DLR,     KC_LCBR,      KC_RCBR,    KC_AT,       KC_TRNS,
-                            KC_PGUP,      KC_PGDN,                     KC_TRNS,      KC_TRNS,    KC_TRNS,           KC_TRNS,        KC_TRNS,    KC_TRNS,                KC_HOME,      KC_END,
-                                                                                     KC_TRNS,    KC_TRNS,           KC_TRNS,        KC_TRNS
-  ),
+    [_SYMBOL] = LAYOUT_5x6_5(
+        KC_TRNS,    KC_F1,      KC_F2,        KC_F3,        KC_F4,         KC_F5,                                                                        KC_F6,      KC_F7,      KC_F8,        KC_F9,      KC_F10,      KC_TRNS,
+        KC_TRNS,    KC_QUOT,    KC_LT,        KC_GT,        KC_DQUO,       KC_HASH,                                                                      KC_AMPR,    DB_COLN,    KC_LBRC,      KC_RBRC,    KC_PERC,     KC_TRNS,
+        KC_TRNS,    KC_EXLM,    KC_MINS,      KC_PLUS,      KC_EQL,        KC_GRV,                                                                       KC_PIPE,    KC_COLN,    KC_LPRN,      KC_RPRN,    KC_QUES,     KC_TRNS,
+        KC_TRNS,    KC_CIRC,    KC_SLSH,      KC_ASTR,      KC_BSLS,       PARENT_DIR,                                                                   KC_TILD,    KC_DLR,     KC_LCBR,      KC_RCBR,    KC_AT,       KC_TRNS,
+                                KC_PGUP,      KC_PGDN,                     KC_TRNS,     KC_TRNS,        KC_TRNS,         KC_TRNS,        KC_TRNS,        KC_TRNS,                KC_HOME,      KC_END,
+                                                                                        KC_TRNS,        KC_TRNS,         KC_TRNS,        KC_TRNS
+    ),
 
-  [_EXTEND] = LAYOUT_5x6_5(
-    KC_TRNS,    KC_NO,      DT_PRNT,     DT_UP,        DT_DOWN,       KC_NO,                                                                   KC_NO,      KC_NO,      KC_BRIU,      KC_MPLY,    KC_VOLU,     KC_NO,
-    KC_TRNS,    KC_NO,      KC_P7,        KC_P8,        KC_P9,         KC_NO,                                                                   KC_NO,      KC_NO,      KC_BRID,      KC_MSTP,    KC_VOLD,     KC_NO,
-    KC_TRNS,    KC_NO,      KC_P4,        KC_P5,        KC_P6,         KC_NO,                                                                   KC_LEFT,    KC_DOWN,    KC_UP,        KC_RGHT,    KC_NO,       KC_NO,
-    KC_TRNS,    KC_NO,      KC_P1,        KC_P2,        KC_P3,         KC_NO,                                                                   KC_NO,      KC_NO,      KC_MS_L,      KC_MS_R,    KC_NO,       KC_NO,
-                            KC_P0,        KC_PDOT,                     KC_TRNS,      KC_TRNS,    KC_TRNS,           KC_TRNS,        KC_TRNS,    KC_TRNS,                KC_NO,        KC_NO,
-                                                                                     KC_TRNS,    KC_TRNS,           KC_TRNS,        KC_TRNS
-  ),
+    [_EXTEND] = LAYOUT_5x6_5(
+        KC_TRNS,    KC_NO,      DT_PRNT,      DT_UP,        DT_DOWN,       KC_NO,                                                                         KC_NO,      KC_NO,      KC_BRIU,      KC_MPLY,    KC_VOLU,     KC_NO,
+        KC_TRNS,    KC_NO,      KC_P7,        KC_P8,        KC_P9,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_BRID,      KC_MSTP,    KC_VOLD,     KC_NO,
+        KC_TRNS,    KC_NO,      KC_P4,        KC_P5,        KC_P6,         KC_NO,                                                                        KC_LEFT,    KC_DOWN,    KC_UP,        KC_RGHT,    KC_NO,       KC_NO,
+        KC_TRNS,    KC_NO,      KC_P1,        KC_P2,        KC_P3,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_MS_L,      KC_MS_R,    KC_NO,       KC_NO,
+                                KC_P0,        KC_PDOT,                     KC_TRNS,    KC_TRNS,        KC_TRNS,         KC_TRNS,        KC_TRNS,         KC_TRNS,                KC_NO,        KC_NO,
+                                                                                       KC_TRNS,        KC_TRNS,         KC_TRNS,        KC_TRNS
+    ),
 
 };
 
