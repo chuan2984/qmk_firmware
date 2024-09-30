9fd4fb6ed051a8ca8676c4fc333459055c93a7b6
chuan2984 <chuanscareer@gmail.com>
2024-09-29 23:15:08 -0700

    feat: add a 4th gaming layer TODO: need to make it not a copy of default lyaer

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/chuan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/chuan/keymap.c
@@ -27,6 +27,7 @@ enum custom_layers {
     _CHUAN = 0,
     _SYMBOL = 1,
     _EXTEND = 2,
+    _GAMING = 3,
 };
 
 enum custom_keycodes {
@@ -91,11 +92,11 @@ void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_CHUAN] = LAYOUT_5x6_5(
-        INV_1P,      KC_1,      KC_2,        KC_3,          KC_4,          KC_5,                                                                         KC_6,        KC_7,      KC_8,         KC_9,       KC_0,        KC_NO,
+        INV_1P,      KC_1,      KC_2,        KC_3,          KC_4,          KC_5,                                                                         KC_6,        KC_7,      KC_8,         KC_9,       KC_0,        TT(_GAMING),
         KC_TAB,      KC_Q,      KC_W,        KC_E,          KC_R,          KC_T,                                                                         KC_Y,        KC_U,      KC_I,         KC_O,       KC_P,        KC_UNDS,
         KC_ESC,      CKC_A,     CKC_S,       CKC_D,         CKC_F,         KC_G,                                                                         KC_H,        CKC_J,     CKC_K,        CKC_L,      CKC_SCLN,    KC_QUOT,
         KC_LSFT,     KC_Z,      KC_X,        KC_C,          KC_V,          KC_B,                                                                         KC_N,        KC_M,      KC_COMM,      KC_DOT,     KC_SLSH,     KC_RSFT,
-                                KC_LBRC,     KC_RBRC,                      KC_BSPC,     KC_LGUI,       TT(_EXTEND),       TT(_EXTEND),    KC_LCTL,       KC_SPC,                 KC_EQL,       KC_DEL,
+                                KC_LBRC,     KC_RBRC,                      KC_BSPC,     KC_LCTL,       TT(_EXTEND),       TT(_EXTEND),    KC_LGUI,       KC_SPC,                 KC_EQL,       KC_DEL,
                                                                                         KC_LALT,       TT(_SYMBOL),       TT(_SYMBOL),    KC_ENT
     ),
 
@@ -109,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_EXTEND] = LAYOUT_5x6_5(
-        KC_TRNS,    KC_NO,      KC_NO,        KC_NO,        KC_NO,         KC_NO,                                                                         KC_NO,      KC_NO,      KC_BRIU,      KC_MPLY,    KC_VOLU,     KC_NO,
+        KC_TRNS,    KC_NO,      KC_NO,        KC_NO,        KC_NO,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_BRIU,      KC_MPLY,    KC_VOLU,     KC_NO,
         KC_TRNS,    KC_NO,      KC_P7,        KC_P8,        KC_P9,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_BRID,      KC_MSTP,    KC_VOLD,     KC_NO,
         KC_TRNS,    KC_NO,      KC_P4,        KC_P5,        KC_P6,         KC_NO,                                                                        KC_LEFT,    KC_DOWN,    KC_UP,        KC_RGHT,    KC_NO,       KC_NO,
         KC_TRNS,    KC_NO,      KC_P1,        KC_P2,        KC_P3,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_MS_L,      KC_MS_R,    KC_NO,       KC_NO,
@@ -117,6 +118,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                        KC_TRNS,        KC_TRNS,         KC_TRNS,        KC_TRNS
     ),
 
+    [_GAMING] = LAYOUT_5x6_5(
+        INV_1P,      KC_1,      KC_2,        KC_3,          KC_4,          KC_5,                                                                         KC_6,        KC_7,      KC_8,         KC_9,       KC_0,        TT(_GAMING),
+        KC_TAB,      KC_Q,      KC_W,        KC_E,          KC_R,          KC_T,                                                                         KC_Y,        KC_U,      KC_I,         KC_O,       KC_P,        KC_UNDS,
+        KC_ESC,      KC_A,      KC_S,        KC_D,          KC_F,          KC_G,                                                                         KC_H,        KC_J,      KC_K,         KC_L,       KC_SCLN,     KC_QUOT,
+        KC_LSFT,     KC_Z,      KC_X,        KC_C,          KC_V,          KC_B,                                                                         KC_N,        KC_M,      KC_COMM,      KC_DOT,     KC_SLSH,     KC_RSFT,
+                                KC_LBRC,     KC_RBRC,                      KC_BSPC,     KC_LCTL,       TT(_EXTEND),       TT(_EXTEND),    KC_LGUI,       KC_SPC,                 KC_EQL,       KC_DEL,
+                                                                                        KC_LALT,       TT(_SYMBOL),       TT(_SYMBOL),    KC_ENT
+    ),
 };
 
 // RGB Modes
@@ -157,10 +166,20 @@ const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {17, 3, HSV_LUNA}
 );
 
+const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 3, HSV_LUNA},
+    {3, 4, HSV_RED},
+    {7, 3, HSV_LUNA},
+    {10, 3, HSV_LUNA},
+    {13, 4, HSV_RED},
+    {17, 3, HSV_LUNA}
+);
+
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_layer0_layer,
     my_layer1_layer,
-    my_layer2_layer
+    my_layer2_layer,
+    my_layer3_layer
 );
 
 void keyboard_post_init_user(void) {
@@ -172,5 +191,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(0, layer_state_cmp(state, _CHUAN));
     rgblight_set_layer_state(1, layer_state_cmp(state, _SYMBOL));
     rgblight_set_layer_state(2, layer_state_cmp(state, _EXTEND));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _GAMING));
     return state;
 }
