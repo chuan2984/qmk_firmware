e6bc9226d1fb62cabde4dad9ee611f9ff169abeb
chuan2984 <chuanscareer@gmail.com>
2024-10-07 20:20:50 -0700

    feat: disable mouse and extra key

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/chuan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/chuan/keymap.c
@@ -124,10 +124,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_EXTEND] = LAYOUT_5x6_5(
-        KC_TRNS,    KC_NO,      KC_NO,        KC_NO,        KC_NO,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_BRIU,      KC_MPLY,    KC_VOLU,     KC_NO,
-        KC_TRNS,    KC_NO,      KC_P7,        KC_P8,        KC_P9,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_BRID,      KC_MSTP,    KC_VOLD,     KC_NO,
+        KC_TRNS,    KC_NO,      KC_NO,        KC_NO,        KC_NO,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_NO,        KC_NO,      KC_NO,       KC_NO,
+        KC_TRNS,    KC_NO,      KC_P7,        KC_P8,        KC_P9,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_NO,        KC_NO,      KC_NO,       KC_NO,
         KC_TRNS,    KC_NO,      KC_P4,        KC_P5,        KC_P6,         KC_NO,                                                                        KC_LEFT,    KC_DOWN,    KC_UP,        KC_RGHT,    KC_NO,       KC_NO,
-        KC_TRNS,    KC_NO,      KC_P1,        KC_P2,        KC_P3,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_MS_L,      KC_MS_R,    KC_NO,       KC_NO,
+        KC_TRNS,    KC_NO,      KC_P1,        KC_P2,        KC_P3,         KC_NO,                                                                        KC_NO,      KC_NO,      KC_NO,        KC_NO,      KC_NO,       KC_NO,
                                 KC_P0,        KC_PDOT,                     KC_TRNS,    KC_TRNS,        KC_TRNS,         KC_TRNS,        KC_TRNS,         KC_TRNS,                KC_NO,        KC_NO,
                                                                                        KC_TRNS,        KC_TRNS,         KC_TRNS,        KC_TRNS
     ),
