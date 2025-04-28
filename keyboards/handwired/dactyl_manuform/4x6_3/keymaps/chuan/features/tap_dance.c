76c1f76ea185b837a61a64b07583b8d62cb566bf
chuan2984 <chuanscareer@gmail.com>
2025-04-28 07:31:52 -0700

    fix: made tapping d easier to register d as opposed to shift

--- keyboards/handwired/dactyl_manuform/4x6_3/keymaps/chuan/features/tap_dance.c
+++ keyboards/handwired/dactyl_manuform/4x6_3/keymaps/chuan/features/tap_dance.c
@@ -4,7 +4,7 @@
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SFT_D:
-            return TAPPING_TERM - 40;
+            return TAPPING_TERM - 20;
         case SFT_K:
             return TAPPING_TERM - 20;
         default:
